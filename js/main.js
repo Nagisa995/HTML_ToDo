cc002e9388eed1c82336db87a13d25dce1d8bd86
Nagisa <miku195@mail.ru>
2022-02-19 01:39:18 +0700

    v2.0 on marker

--- js/main.js
+++ js/main.js
@@ -1,74 +1,95 @@
-let operator = '', previousNumber = 0;
-
 function screenSize(container) {
-    if (container.length > 9) calcScreen.style.fontSize = '32px';
+    if (container.length > 8) calcScreen.style.fontSize = '32px';
     else {
         if (container.length > 5) calcScreen.style.fontSize = '64px';
         else calcScreen.style.fontSize = '96px';
     }
 }
 
-function addNumber() {
-    if(calcScreen.textContent === 'Error') return;
-    if (calcScreen.textContent === '0' || calcScreen.textContent == previousNumber)
-        calcScreen.textContent = this.textContent;
-    else calcScreen.textContent += this.textContent;
+function setMarker(event) {
+    let lastEvent = document.querySelector('div.Event');
+    if (lastEvent !== null) lastEvent.classList.remove('Event');
+    event.currentTarget.classList.add('Event');
+    console.log(lastEvent);
+}
+
+function addNumber(event) {
+    if (calcScreen.textContent === 'Error') return;
+    let lastEvent = document.querySelector('div.Event');
+    if (calcScreen.textContent === '0' || lastEvent.classList.contains('block_operation'))
+        calcScreen.textContent = event.currentTarget.textContent;
+    else
+        calcScreen.textContent += +event.currentTarget.textContent;
     screenSize(calcScreen.textContent);
+    setMarker(event);
 }
 
-function operation() {
-    if(calcScreen.textContent === 'Error') return;
-    if (operator === '')
-        previousNumber = calcScreen.textContent;
-    else 
-        result();
-    operator = this.id;
+function procedure(event) {  
+    let lastEvent = document.querySelector('div.Event');
+    if (lastEvent.classList.contains('block_operation')) {
+        if (lastEvent.textContent!=='=') {
+            operator = event.currentTarget.id;
+            setMarker(event);
+            return;
+        }
+    }
+    if (previousNumber !== ''&&lastEvent.textContent!==backspace.textContent) calculation(event);
+    previousNumber = calcScreen.textContent;
+    operator = event.currentTarget.id;
+    setMarker(event);
 }
 
-function result() {
-    let resultCalc =
+function calculation(event) {
+    let lastEvent = document.querySelector('div.Event');
+    if (lastEvent.classList.contains('block_operation')) return;
+    if (calcScreen.textContent === '0' || calcScreen.textContent === 'Error') {
+        calcScreen.textContent = 'Error';
+        return;
+    }
+    const resultCalc =
     {
         sum: +previousNumber + (+calcScreen.textContent),
         sub: previousNumber - calcScreen.textContent,
         mult: previousNumber * calcScreen.textContent,
         div: previousNumber / calcScreen.textContent,
     };
-    if (calcScreen.textContent == '0' || operator === '') {
-        clearScreen();
-        calcScreen.textContent = 'Error';
-        return;
-    }
-    let solution = resultCalc[operator];
-    previousNumber = solution;
-    calcScreen.textContent = solution;
+    calcScreen.textContent = resultCalc[operator];
     screenSize(calcScreen.textContent);
-    operator = '';
+    setMarker(event);
+};
+
+function clearScreen(event){
+    calcScreen.textContent='0';
+    previousNumber='';
+    operator=undefined;
+    screenSize(calcScreen.textContent);
+    setMarker(event);
 }
 
-function clearScreen() {
-    calcScreen.textContent = '0';
-    operator = '';
-    previousNumber = 0;
-}
-
-function DELETE() {
-    if (calcScreen.textContent === 'Error') return;
+function backSpace(event){
+    if (calcScreen.textContent === 'Error') {
+        clearScreen(event);
+        return};
     calcScreen.textContent = calcScreen.textContent.slice(0, -1);
     if (calcScreen.textContent === '') calcScreen.textContent = 0;
     screenSize(calcScreen.textContent);
+    setMarker(event);
 }
 
-let k = document.querySelectorAll('div.block_item');
-for (let elem of k) {
+let previousNumber= '' , operator;
+console.log(operator);
+
+let button = document.querySelectorAll('div.block_item');
+for (let elem of button) {
     elem.addEventListener('click', addNumber);
 }
-del.removeEventListener('click', addNumber);
-k = document.querySelectorAll('div.block_operation');
-for (let elem of k) {
-    elem.addEventListener('click', operation);
-}
-RESULT.removeEventListener('click', operation);
-del.addEventListener('click', DELETE);
-CLEAR.addEventListener('click', clearScreen);
-RESULT.addEventListener('click', result);
 document.querySelector('div.block_zero').addEventListener('click', addNumber);
+backspace.removeEventListener('click', addNumber);
+backspace.addEventListener('click', backSpace);
+button = document.querySelectorAll('div.block_operation');
+for (let elem of button) {
+    elem.addEventListener('click', procedure);
+}
+RESULT.removeEventListener('click', procedure);
+RESULT.addEventListener('click', calculation);
+CLEAR.addEventListener('click', clearScreen);
